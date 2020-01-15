70ae7b025fe074d8daa1ebf6f169b8f20e37f77c
mesheryctl releaser <community@layer5.io>
2020-01-14 22:36:19 -0600

    Brew formula update for mesheryctl version v0.3.7

--- Formula/mesheryctl.rb
+++ Formula/mesheryctl.rb
@@ -2,16 +2,16 @@
 class Mesheryctl < Formula
   desc "The Multi-Service Mesh Management Plane."
   homepage "https://layer5.io/meshery"
-  version "0.3.6"
+  version "0.3.7"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/layer5io/meshery/releases/download/v0.3.6/mesheryctl_0.3.6_Darwin_x86_64.zip"
-    sha256 "67b6861b4c5938838a7a707fe2076ca5102b7a05586321ffc054e993d8bc30e4"
+    url "https://github.com/layer5io/meshery/releases/download/v0.3.7/mesheryctl_0.3.7_Darwin_x86_64.zip"
+    sha256 "31f7ef235162b2ff799cf25e83dc2af8046f3d11e254ddb13a06555b8128c931"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/layer5io/meshery/releases/download/v0.3.6/mesheryctl_0.3.6_Linux_x86_64.zip"
-      sha256 "c6225b56902da08f6e060edcf82167c44e19b2b018011444be91fe4488e11091"
+      url "https://github.com/layer5io/meshery/releases/download/v0.3.7/mesheryctl_0.3.7_Linux_x86_64.zip"
+      sha256 "bc00308a8825d8a1457f9f62b8527cfc1fc8e9a847dfae6f5b60d0f9a5f9a77b"
     end
   end
 
