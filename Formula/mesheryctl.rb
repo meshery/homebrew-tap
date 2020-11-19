385100aab6b5aa4ff88c787061770feb814d3d51
mesheryctl releaser <community@layer5.io>
2020-11-19 07:13:48 -0600

    Brew formula update for mesheryctl version v0.4.23

--- Formula/mesheryctl.rb
+++ Formula/mesheryctl.rb
@@ -2,26 +2,24 @@
 class Mesheryctl < Formula
   desc "The Multi-Service Mesh Management Plane."
   homepage "https://layer5.io/meshery"
-  version "0.4.22"
+  version "0.4.23"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/layer5io/meshery/releases/download/v0.4.22/mesheryctl_0.4.22_Darwin_x86_64.zip"
-    sha256 "f12fdd7ed1bc01aa36170fa8ca9d5cde2e844824223cb032aa531914449ab912"
-  elsif OS.linux?
-    if Hardware::CPU.intel?
-      url "https://github.com/layer5io/meshery/releases/download/v0.4.22/mesheryctl_0.4.22_Linux_x86_64.zip"
-      sha256 "1c90053544e3d739ed5ee1e1dc5d266cd4ef62f70e6e8ff4de8c5dc99135fd2e"
-    end
-    if Hardware::CPU.arm?
-      if Hardware::CPU.is_64_bit?
-        url "https://github.com/layer5io/meshery/releases/download/v0.4.22/mesheryctl_0.4.22_Linux_arm64.zip"
-        sha256 "fdd8da299fda41f1f44534d8b6e7b001b7d159e6524aa9c1adbb6beec881c7cd"
-      else
-        url "https://github.com/layer5io/meshery/releases/download/v0.4.22/mesheryctl_0.4.22_Linux_armv6.zip"
-        sha256 "4bc90c86808b3714b2c1a586c93cca0960cb2e4d9e117fc37771012799eae742"
-      end
-    end
+    url "https://github.com/layer5io/meshery/releases/download/v0.4.23/mesheryctl_0.4.23_Darwin_x86_64.zip"
+    sha256 "b4b427d707c554c69ec5afcc2f8543b28493e2be885ce76977ab4018407c3e0c"
+  end
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/layer5io/meshery/releases/download/v0.4.23/mesheryctl_0.4.23_Linux_x86_64.zip"
+    sha256 "f4b3e21b3ed82f3facf177cc0715dab40a5d1efead70617a41b74e2d5bc7fc32"
+  end
+  if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+    url "https://github.com/layer5io/meshery/releases/download/v0.4.23/mesheryctl_0.4.23_Linux_armv6.zip"
+    sha256 "209c3476aa39786b6e4cbb256e159b8f5f47aec211f1f974a8264329e7249ee1"
+  end
+  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+    url "https://github.com/layer5io/meshery/releases/download/v0.4.23/mesheryctl_0.4.23_Linux_arm64.zip"
+    sha256 "c1f48700480ace28b1f3603b1bda7731b370452c9fd11c16b3119fd6ea778df6"
   end
 
   def install
