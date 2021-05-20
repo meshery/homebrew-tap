8805576fa56084e7e5580788471740b887938169
mesheryctl releaser <community@layer5.io>
2021-05-20 14:41:58 -0500

    Brew formula update for mesheryctl version v0.5.14

--- Formula/mesheryctl.rb
+++ Formula/mesheryctl.rb
@@ -5,24 +5,24 @@
 class Mesheryctl < Formula
   desc "The Multi-Service Mesh Management Plane."
   homepage "https://layer5.io/meshery"
-  version "0.5.13"
+  version "0.5.14"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/layer5io/meshery/releases/download/v0.5.13/mesheryctl_0.5.13_Darwin_x86_64.zip"
-    sha256 "7ee4f653f87b9a70856ab009715039925c80673c5a06a3b169ee92451aba5f5b"
+    url "https://github.com/layer5io/meshery/releases/download/v0.5.14/mesheryctl_0.5.14_Darwin_x86_64.zip"
+    sha256 "394d316c999b84b5a8769edf384e24162a68564d75e8d3212aff9b94e0348ee5"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/layer5io/meshery/releases/download/v0.5.13/mesheryctl_0.5.13_Linux_x86_64.zip"
-    sha256 "c40da11a6b65c1e5cb0ca641764a76ce21e8b6c93aac954fcd5d44d42cf1de90"
+    url "https://github.com/layer5io/meshery/releases/download/v0.5.14/mesheryctl_0.5.14_Linux_x86_64.zip"
+    sha256 "32978f7140d18543547406c51fe4f94e0466a90926f869741cd2f6c045cd1606"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/layer5io/meshery/releases/download/v0.5.13/mesheryctl_0.5.13_Linux_armv6.zip"
-    sha256 "09da6e8206a8ea2839a523162474cb931270d7f0aab01db4c5e985371db462bb"
+    url "https://github.com/layer5io/meshery/releases/download/v0.5.14/mesheryctl_0.5.14_Linux_armv6.zip"
+    sha256 "453ad62a195fe08b44ebb79d93a79b324bc10d46e5ce1377bc0c9b8c27b8da4b"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/layer5io/meshery/releases/download/v0.5.13/mesheryctl_0.5.13_Linux_arm64.zip"
-    sha256 "0d0ac6f2ad9978e0a77b12c09c1d480540fc845a826653db269d7d803b840a0b"
+    url "https://github.com/layer5io/meshery/releases/download/v0.5.14/mesheryctl_0.5.14_Linux_arm64.zip"
+    sha256 "77ddedf68fc7005403f6ba62e1ce36595077f42a05b4fbfd63ce0457d3ea5bec"
   end
 
   def install
